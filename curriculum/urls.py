b6c3623bb7c0a436410acb9c751179f3d098ac79
Luke Burns <lbburns13@gmail.com>
2017-04-26 16:54:29 +0100

    Added marketing detail/update view

--- curriculum/urls.py
+++ curriculum/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # /curriculum/offering/add
     url(r'^offering/add/$', views.OfferingCreate.as_view(), name='offering-add'),
-    # /curriculum/offering/add
+    # /curriculum/offering/<id>
     url(r'^offering/(?P<pk>[0-9]+)/$', views.OfferingUpdate.as_view(), name='offering-update'),
-    # /curriculum/offering/add
+    # /curriculum/offering/<id>/delete
     url(r'^offering/(?P<pk>[0-9]+)/delete/$', views.OfferingDelete.as_view(), name='offering-delete'),
+    # /curriculum/offering/<id>/marketing
+    url(r'^offering/(?P<pk>[0-9]+)/marketing/$', views.MarketingUpdate.as_view(), name='marketing-update'),
 ]
--- curriculum/views.py
+++ curriculum/views.py
@@ -21,6 +21,10 @@ class OfferingUpdate(UpdateView):
     model = Offering
     fields = ['course_caution','course_name','course_delivery','course_type','parent_code','strand','department','location','qual_aim','fee_comments','start_date','end_date','start_time','end_time','day','study_year','study_year_duration','wk_hrs','number_wks','glh','number_grps','min_students','max_students','mkt_course_name','mkt_course_overview','mkt_prospectus','mkt_entry_req','mkt_topics','mkt_target','mkt_learning','mkt_assess','mkt_career','mkt_funding','mkt_add_costs','mkt_where','mkt_bring','mkt_other','mkt_tutor',]
 
+class MarketingUpdate(UpdateView):
+    model = Offering
+    fields = ['mkt_course_name','mkt_course_overview','mkt_prospectus','mkt_entry_req','mkt_topics','mkt_target','mkt_learning','mkt_assess','mkt_career','mkt_funding','mkt_add_costs','mkt_where','mkt_bring','mkt_other','mkt_tutor',]
+
 class OfferingDelete(DeleteView):
     model = Offering
     success_url = reverse_lazy('curriculum:index')
